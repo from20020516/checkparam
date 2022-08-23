41b10cbb05ba36a61eb046a94971e192ee39368c
ajiyoshi-vg <Yoichi_Sudo@voyagegroup.com>
2022-08-23 14:03:47 +0900

    ...

--- src/query.ts
+++ src/query.ts
@@ -9,7 +9,7 @@ export function Apply(cond: Condition, item: Item[]): Item[] {
 }
 
 const FilterSet = (cond: Condition): Filter =>
-  composeAnd(
+  every(
     [
       cond.text
         .split(/\s/)
@@ -21,16 +21,16 @@ const FilterSet = (cond: Condition): Filter =>
     ].flatMap(xs => xs)
   );
 
-const composeAnd = (fs: Filter[]): Filter => {
+const every = (fs: Filter[]): Filter => {
   return (item: Item): boolean => fs.every(accept => accept(item));
 };
-const composeOr = (fs: Filter[]): Filter => {
+const some = (fs: Filter[]): Filter => {
   return (item: Item): boolean => fs.some(accept => accept(item));
 };
 
 type extractor<T> = (item: Item) => T | null;
 
-const composeExtract = <T>(ext: extractor<T>, f: (x: T) => boolean): Filter => {
+const extractThen = <T>(ext: extractor<T>, f: (x: T) => boolean): Filter => {
   return (item: Item): boolean => {
     const x = ext(item);
     return x ? f(x) : false;
@@ -43,45 +43,48 @@ const textFilter = (raw: string): Filter => {
   if (gte) {
     const prop = propValue(gte[1]);
     const threshold = Number(gte[2]);
-    return composeExtract(prop, x => x >= threshold);
+    return extractThen(prop, x => x >= threshold);
   }
   const gt = word.match(/^(.*)>([-+]?\d+)$/);
   if (gt) {
     const prop = propValue(gt[1]);
     const threshold = Number(gt[2]);
-    return composeExtract(prop, x => x > threshold);
+    return extractThen(prop, x => x > threshold);
   }
   const lte = word.match(/^(.*)<=([-+]?\d+)$/);
   if (lte) {
     const prop = propValue(lte[1]);
     const threshold = Number(lte[2]);
-    return composeExtract(prop, x => x <= threshold);
+    return extractThen(prop, x => x <= threshold);
   }
   const lt = word.match(/^(.*)<([-+]?\d+)$/);
   if (lt) {
     const prop = propValue(lt[1]);
     const threshold = Number(lt[2]);
-    return composeExtract(prop, x => x < threshold);
+    return extractThen(prop, x => x < threshold);
   }
   const eq = word.match(/^(.*)=([-+]?\d+)$/);
   if (eq) {
     const prop = propValue(eq[1]);
     const value = Number(eq[2]);
-    return composeExtract(prop, x => x === value);
+    return extractThen(prop, x => x === value);
   }
   const not = word.match(/^-(.*)$/);
   if (not) {
     const val = not[1];
-    return composeExtract(wholeTextLowerCase, lc => !lc.includes(val.toLowerCase()));
+    return extractThen(
+      wholeTextLowerCase,
+      lc => !lc.includes(val.toLowerCase())
+    );
   }
-  return composeExtract(wholeTextLowerCase, lc => lc.includes(word.toLowerCase()));
+  return extractThen(wholeTextLowerCase, lc => lc.includes(word.toLowerCase()));
 };
 
 const acceptAlways = () => true;
 
 const jobFilter = (cond: Condition): Filter =>
   cond.job_flags > 0
-    ? composeExtract(
+    ? extractThen(
         item => item._jobs,
         that => (that & cond.job_flags) > 0
       )
@@ -91,7 +94,7 @@ const typeFilter = (cond: Condition): Filter => {
   const fs = [
     cond.slot_flags > 0
       ? [
-          composeExtract(
+          extractThen(
             item => item._slots,
             that => (that & cond.slot_flags) > 0
           ),
@@ -99,19 +102,19 @@ const typeFilter = (cond: Condition): Filter => {
       : [],
     cond.skill.size > 0
       ? [
-          composeExtract(
+          extractThen(
             item => item.skill,
             id => cond.skill.has(id)
           ),
         ]
       : [],
   ].flatMap(xs => xs);
-  return fs.length > 0 ? composeOr(fs) : acceptAlways;
+  return fs.length > 0 ? some(fs) : acceptAlways;
 };
 
 const levelFilter = (cond: Condition): Filter =>
   cond.minLevel
-    ? composeExtract(
+    ? extractThen(
         item => (item.item_level > 0 ? item.item_level : item.level),
         that => that >= cond.minLevel
       )
@@ -125,4 +128,5 @@ export function propValue(prop: string): extractor<number> {
   };
 }
 
-const wholeTextLowerCase: extractor<string> = (item: Item) => [item.name, item.description].join().toLowerCase();
+const wholeTextLowerCase: extractor<string> = (item: Item) =>
+  [item.name, item.description].join().toLowerCase();
