c297418cb9a4b5746188de2129453847581a2448
ajiyoshi-vg <Yoichi_Sudo@voyagegroup.com>
2022-08-23 11:30:41 +0900

    ...

--- src/query.ts
+++ src/query.ts
@@ -2,95 +2,120 @@ import { Item } from '../utils';
 import { Condition } from './condition';
 import { normalize } from './constants';
 
+type Filter = (item: Item) => boolean;
+
 export function Apply(cond: Condition, item: Item[]): Item[] {
-  const f = new FilterSet(cond);
-  return item.filter(x => f.accept(x));
+  return item.filter(FilterSet(cond));
 }
 
-interface FilterInterface {
-  accept(item: Item): boolean;
-}
+const FilterSet = (cond: Condition): Filter =>
+  composeAnd(
+    [
+      cond.text
+        .split(/\s/)
+        .filter(x => x !== '')
+        .map(x => textFilter(x)),
+      [jobFilter(cond)],
+      [typeFilter(cond)],
+      [levelFilter(cond)],
+    ].flatMap(xs => xs)
+  );
 
-class AllOK implements FilterInterface {
-  accept(item: Item): boolean {
-    return true;
-  }
-}
+const composeAnd = (fs: Filter[]): Filter => {
+  return (item: Item): boolean => fs.every(accept => accept(item));
+};
+const composeOr = (fs: Filter[]): Filter => {
+  return (item: Item): boolean => fs.some(accept => accept(item));
+};
 
-const allOK = new AllOK();
+type extractor<T> = (item: Item) => T | null;
 
-class FilterSet implements FilterInterface {
-  private fs: FilterInterface[];
+const composeExtract = <T>(ext: extractor<T>, f: (x: T) => boolean): Filter => {
+  return (item: Item): boolean => {
+    const x = ext(item);
+    return x ? f(x) : false;
+  };
+};
 
-  constructor(cond: Condition) {
-    this.fs = [
-      ...cond.text.split(/\s/).map(x => textFilter(x)),
-      cond.job_flags ? jobFilter(cond.job_flags) : allOK,
-      kindFilter(cond),
-      cond.minLevel ? minLevelFilter(cond.minLevel) : allOK,
-    ];
-  }
+const acceptAlways = () => true;
 
-  accept(item: Item): boolean {
-    return this.fs.every((p: FilterInterface) => p.accept(item));
-  }
-}
-
-function textFilter(raw: string): FilterInterface {
+const textFilter = (raw: string): Filter => {
   const word = normalize(raw);
   const gte = word.match(/^(.*)>=([-+]?\d+)$/);
   if (gte) {
     const prop = propValue(gte[1]);
     const threshold = Number(gte[2]);
-    return new Filter(prop, x => x >= threshold);
+    return composeExtract(prop, x => x >= threshold);
   }
   const gt = word.match(/^(.*)>([-+]?\d+)$/);
   if (gt) {
     const prop = propValue(gt[1]);
     const threshold = Number(gt[2]);
-    return new Filter(prop, x => x > threshold);
+    return composeExtract(prop, x => x > threshold);
   }
   const lte = word.match(/^(.*)<=([-+]?\d+)$/);
   if (lte) {
     const prop = propValue(lte[1]);
     const threshold = Number(lte[2]);
-    return new Filter(prop, x => x <= threshold);
+    return composeExtract(prop, x => x <= threshold);
   }
   const lt = word.match(/^(.*)<([-+]?\d+)$/);
   if (lt) {
     const prop = propValue(lt[1]);
     const threshold = Number(lt[2]);
-    return new Filter(prop, x => x < threshold);
+    return composeExtract(prop, x => x < threshold);
   }
   const eq = word.match(/^(.*)=([-+]?\d+)$/);
   if (eq) {
     const prop = propValue(eq[1]);
     const value = Number(eq[2]);
-    return new Filter(prop, x => x === value);
+    return composeExtract(prop, x => x === value);
   }
   const not = word.match(/^-(.*)$/);
   if (not) {
     const val = not[1];
-    return new Filter(wholeTextLowerCase, lc => !lc.includes(val.toLowerCase()));
+    return composeExtract(wholeTextLowerCase, lc => !lc.includes(val.toLowerCase()));
   }
-  return new Filter(wholeTextLowerCase, lc => lc.includes(word.toLowerCase()));
-}
+  return composeExtract(wholeTextLowerCase, lc => lc.includes(word.toLowerCase()));
+};
 
-type extractor<T> = (item: Item) => T | null;
+const jobFilter = (cond: Condition): Filter =>
+  cond.job_flags > 0
+    ? composeExtract(
+        item => item._jobs,
+        that => (that & cond.job_flags) > 0
+      )
+    : acceptAlways;
 
-class Filter<T> implements FilterInterface {
-  extract: extractor<T>;
-  f: (x: T) => boolean;
-  constructor(ext: extractor<T>, f: (x: T) => boolean) {
-    this.extract = ext;
-    this.f = f;
-  }
+const typeFilter = (cond: Condition): Filter => {
+  const fs = [
+    cond.slot_flags > 0
+      ? [
+          composeExtract(
+            item => item._slots,
+            that => (that & cond.slot_flags) > 0
+          ),
+        ]
+      : [],
+    cond.skill.size > 0
+      ? [
+          composeExtract(
+            item => item.skill,
+            id => cond.skill.has(id)
+          ),
+        ]
+      : [],
+  ].flatMap(xs => xs);
+  return fs.length > 0 ? composeOr(fs) : acceptAlways;
+};
 
-  accept(item: Item): boolean {
-    const x = this.extract(item);
-    return x ? this.f(x) : false;
-  }
-}
+const levelFilter = (cond: Condition): Filter =>
+  cond.minLevel
+    ? composeExtract(
+        item => (item.item_level > 0 ? item.item_level : item.level),
+        that => that >= cond.minLevel
+      )
+    : acceptAlways;
 
 export function propValue(prop: string): extractor<number> {
   const re = new RegExp(`(^|\\s)${prop}([-+]?\\d+)`, 'i');
@@ -101,57 +126,3 @@ export function propValue(prop: string): extractor<number> {
 }
 
 const wholeTextLowerCase: extractor<string> = (item: Item) => [item.name, item.description].join().toLowerCase();
-
-function jobFilter(job_flags: number): Filter<number> {
-  return new Filter(
-    item => item._jobs,
-    that => (that & job_flags) > 0
-  );
-}
-
-function slotFilter(slot_flags: number): Filter<number> {
-  return new Filter(
-    item => item._slots,
-    that => (that & slot_flags) > 0
-  );
-}
-
-function skillFilter(skill: Set<number>): Filter<number> {
-  return new Filter(
-    item => item.skill,
-    id => (skill.size > 0 ? skill.has(id) : true)
-  );
-}
-
-function minLevelFilter(level: number): Filter<number> {
-  return new Filter(
-    item => (item.item_level > 0 ? item.item_level : item.level),
-    that => that >= level
-  );
-}
-
-function kindFilter(cond: Condition): FilterInterface {
-  if (cond.slot_flags && cond.skill.size > 0) {
-    return new Or(slotFilter(cond.slot_flags), skillFilter(cond.skill));
-  }
-  if (cond.slot_flags) {
-    return slotFilter(cond.slot_flags);
-  }
-  if (cond.skill.size > 0) {
-    return skillFilter(cond.skill);
-  }
-  return allOK;
-}
-
-class Or implements FilterInterface {
-  f: FilterInterface;
-  g: FilterInterface;
-
-  constructor(f: FilterInterface, g: FilterInterface) {
-    this.f = f;
-    this.g = g;
-  }
-  accept(item: Item): boolean {
-    return this.f.accept(item) || this.g.accept(item);
-  }
-}
